54b23b10d2f53211c64135060817ad9e66774f6a
ph4u <1057021+ph4un00b@users.noreply.github.com>
2022-12-27 20:05:33 -0600

    feat(Root): skia and gestures

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Blur, Canvas, Circle, Group } from "@shopify/react-native-skia";
+import React from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  View,
+  Text,
+  StyleSheet,
+} from "react-native";
 
-export default function App() {
+import { Colors, Header } from "react-native/Libraries/NewAppScreen";
+
+const App = () => {
+  const size = 256;
+  const r = size * 0.33;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Canvas style={{ flex: 1 }}>
+      <Group blendMode="multiply">
+        <Circle cx={r} cy={r} r={r} color="cyan" />
+        <Circle cx={size - r} cy={r} r={r} color="magenta" />
+        <Circle
+          cx={size/2}
+          cy={size - r}
+          r={r}
+          color="yellow"
+        />
+      </Group>
+    </Canvas>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;

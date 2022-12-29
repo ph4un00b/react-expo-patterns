92541ccdceed8b7e57bae70458c498f93d33c8b8
ph4u <1057021+ph4un00b@users.noreply.github.com>
2022-12-28 20:55:43 -0600

    chore(Root): handle drawer programatically

--- App.tsx
+++ App.tsx
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { ComponentType, RefAttributes, useRef } from "react";
 import { View, Dimensions, Text, I18nManager, Button } from "react-native";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import {
+  GestureHandlerRootView,
+  PanGestureHandlerProps,
+  TouchableOpacity,
+} from "react-native-gesture-handler";
 import DrawerLayout from "react-native-gesture-handler/DrawerLayout";
 import { NativeRouter, Route, Routes, useNavigate } from "react-router-native";
 import { SkiaScreen } from "./mobile/skia.screen";
 import uuid from "react-native-uuid";
 import { DragScreen } from "./mobile/drag.screen";
 import { PicScreen } from "./mobile/pic.screen";
+import { MaterialCommunityIcons, Feather } from "@expo/vector-icons";
 
 const APP_LINKS = [
-  { uuid: uuid.v4(), path: "/", alias: "home", element: <PicScreen /> },
+  { uuid: uuid.v4(), path: "/", alias: "home", element: <DragScreen /> },
   {
     uuid: uuid.v4(),
     path: "/gesture",
     alias: "gesture",
     element: <DragScreen />,
   },
-  { uuid: uuid.v4(), path: "/skia", alias: "skia", element: <SkiaScreen /> },
-  { uuid: uuid.v4(), path: "/r3f", alias: "r3f", element: <></> },
+  // { uuid: uuid.v4(), path: "/skia", alias: "skia", element: <SkiaScreen /> },
+  // { uuid: uuid.v4(), path: "/r3f", alias: "r3f", element: <></> },
 ];
 
 export default function App() {
@@ -32,12 +37,18 @@ export default function App() {
 
 function RoutesConfig() {
   const navigate = useNavigate();
+  const drawer = useRef<DrawerLayout>(null!);
   return (
     <DrawerLayout
+      ref={drawer}
+      /** @see https://reactnative.dev/docs/drawerlayoutandroid.html#drawerlockmode */
+      drawerLockMode="locked-open"
       // ios only
       enableTrackpadTwoFingerGesture
+      minSwipeDistance={320}
+      userSelect="text"
       edgeWidth={100}
-      drawerWidth={200}
+      drawerWidth={300}
       drawerPosition={I18nManager.isRTL ? "right" : "left"}
       drawerType="front"
       drawerBackgroundColor={"#b3b3b3"}
@@ -49,6 +60,15 @@ function RoutesConfig() {
             alignContent: "center",
           }}
         >
+          <View>
+            <TouchableOpacity onPress={() => drawer.current.closeDrawer()}>
+              <MaterialCommunityIcons
+                name="backburger"
+                size={64}
+                color="black"
+              />
+            </TouchableOpacity>
+          </View>
           <Text style={{ textAlign: "center" }}>Links</Text>
           {APP_LINKS.map((link) => {
             return (
@@ -64,6 +84,11 @@ function RoutesConfig() {
       )}
       onDrawerSlide={(status) => console.log(status)}
     >
+      <View>
+        <TouchableOpacity onPress={() => drawer.current.openDrawer()}>
+          <MaterialCommunityIcons name="backburger" size={64} color="black" />
+        </TouchableOpacity>
+      </View>
       <Routes>
         {APP_LINKS.map((link) => {
           return (

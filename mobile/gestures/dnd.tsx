52f8b9089b923a020cc6dc64cad28f69d4de4fc4
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-01-01 17:19:10 -0600

    feat(Root): drag options

--- mobile/gestures/dnd.tsx
+++ mobile/gestures/dnd.tsx
@@ -10,6 +10,8 @@ import Animated, {
   withDecay,
 } from "react-native-reanimated";
 import { clamp } from "react-native-redash";
+import { Pressable, Text, TouchableWithoutFeedback } from "react-native";
+import { useState } from "react";
 
 type DragProps = {
   children?: React.ReactNode;
@@ -79,6 +81,10 @@ export function DragReanimated({
   decay = false,
 }: DragProps) {
   if (!width || !height) throw new Error("you forgot to pass dimensions!");
+  const [openOpts, setOpenOpts] = useState(false);
+  const [decayOp, setDecayOp] = useState(decay);
+  const [decay2Op, setDecay2Op] = useState(decay);
+  const [decay3Op, setDecay3Op] = useState(decay);
   // const move = useSharedValue({ x: 0, y: 0 });
   const mx = useSharedValue(0);
   const my = useSharedValue(0);
@@ -99,7 +105,7 @@ export function DragReanimated({
       my.value = clamp(e.translationY + ctx.offsetY, -boundY, boundY);
     },
     onEnd: (e) => {
-      if (!decay) return;
+      if (!decayOp) return;
       mx.value = withDecay({ velocity: e.velocityX, clamp: [-boundX, boundX] });
       my.value = withDecay({ velocity: e.velocityY, clamp: [-boundY, boundY] });
       // move.value.x = withDecay({ velocity: e.velocityX, clamp: [0, width - 50] });
@@ -108,7 +114,7 @@ export function DragReanimated({
     },
   });
 
-  const animatedStyles = useAnimatedStyle(() => {
+  const dragStyles = useAnimatedStyle(() => {
     return {
       transform: [
         { translateX: mx.value },
@@ -123,7 +129,7 @@ export function DragReanimated({
     <PanGestureHandler onGestureEvent={drag}>
       <Animated.View
         style={[
-          animatedStyles,
+          dragStyles,
           {
             // aspectRatio: 1,
             borderColor: "purple",
@@ -131,6 +137,41 @@ export function DragReanimated({
           },
         ]}
       >
+        {/*
+         * @see https://docs.swmansion.com/react-native-gesture-handler/docs/api/components/touchables/
+         * TouchableWithoutFeedback from reanimated
+         * seems to not respect z-index
+         * todo: research more!
+         * */}
+        <TouchableWithoutFeedback onLongPress={() => setOpenOpts(!openOpts)}>
+          <Animated.View className="z-10">
+            {/* @see https://reactnative.dev/docs/stylesheet.html#absolutefill-vs-absolutefillobject */}
+            <Pressable onPress={() => setDecayOp(!decayOp)}>
+              <Text
+                className="w-18 absolute -top-4 left-0 rounded-lg border-3 border-slate-900 shadow-lg px-4 transform origin-[0%_50%]"
+                style={{
+                  /** || -24deg */
+                  transform: [{ rotate: openOpts ? "336deg" : "0deg" }],
+                  backgroundColor: decayOp ? "peru" : "",
+                }}
+              >
+                decay
+              </Text>
+            </Pressable>
+            <Text
+              className="bg-slate-200 w-17 absolute -top-4 left-0  rounded-md border-2 border-slate-800 shadow-md px-3 transform origin-[0%_50%]"
+              style={{ transform: [{ rotate: openOpts ? "24deg" : "0deg" }] }}
+            >
+              decay
+            </Text>
+            <Text
+              className="bg-slate-300 w-16 absolute -top-4 rounded-sm border-1 border-slate-700 shadow-sm px-2 transform origin-[0%_50%]"
+              style={{ transform: [{ rotate: openOpts ? "0deg" : "0deg" }] }}
+            >
+              -decay
+            </Text>
+          </Animated.View>
+        </TouchableWithoutFeedback>
         {children}
       </Animated.View>
     </PanGestureHandler>

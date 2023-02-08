ddf1531d1fb6b0fbe9d2c0339db2a5e9065af6f8
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-02-07 19:23:01 -0600

    chore

--- mobile/xstate.sortable.ts
+++ mobile/xstate.sortable.ts
@@ -1,13 +1,19 @@
+import Animated, { useSharedValue } from "react-native-reanimated";
 import { createMachine } from "xstate";
 export const sortableMachine =
   /** @xstate-layout N4IgpgJg5mDOIC5QGUD2AnALgQwEYBswACASUzAFsA6CdbKGuqKASwDsoBiMNiAbQAMAXUSgADqlgtMLVG1EgAHogCsAgOxUAzAK0A2LSvXrdATlPqVAGhABPRPq1UALKb0AmQwEYAHOr0+AioAvsE2aFh4hKTk1LT0jPSsHJwArmIQ2OSCIkggElIycgrKCFpa7lQ+FuruegI+Kiru6lo29ggtXlTqXgF6zp7OFVo+oeEYOATEZJSJDABm7CywABbsXJh0bIWybAC0PPzCCgXSeyWI7qaVzs6WJqYqjfWt7YheKqZUKvWDWk93Pd1OMQBEptFZnEmFQWBBCJxYDgsDlTpJzsU8qUvDoejivqNPuUtJZ3mV-FU3D4AXpeiYaaDwVEZrEqGcimxYWxzth8JxUXl2RcsR9TN0vLoBL4NGpWs4yV9NFocYEgq11OZ3KEwiA2KgIHAFEzpjFKGjdpjQKV9kCyfsvJobipnF5nNTXACniCdcbIaz4lBzRj5CLOvK7Ig9N9fqMfBKjM4PHpGZNmaboQkA8lA4L0RzLmUWlUCT4hr9qVpwx1PT0fM4jCTTPo9B4Qj7UyaofMqEtuWsNkH86G9DiqLSgdUKk9qV4yZ9oyplT5aY13AJ196JpFO-6YXDCIPhVbELU50YqEElyvmuuNCnt365kLLfk80elFcq6oGheDKY7uo9wCJ4Xj3hCLJPm+chcjy+CHi+pSnhGCBfCoPyLu4LTNL89ZjO2D4QdQz6cmwYAAO7wSGx5lIElIjnoBJuPUs7Iah6EVFh7g4c82rBEAA */
   createMachine({
     predictableActionArguments: true,
+    preserveActionOrder: true,
     id: "Sortable Item",
     tsTypes: {} as import("./xstate.sortable.typegen").Typegen0,
 
     schema: {
-      context: {} as {},
+      context: {} as {
+        safeY: number;
+        x: number;
+        y: number;
+      },
       events: {} as
         | { type: "end" }
         | { type: "transition-end" }
@@ -15,7 +21,9 @@ export const sortableMachine =
         | { type: "start" },
     },
 
-    context: {},
+    // context: {
+    //   safeY: useSharedValue(0),
+    // },
 
     type: "parallel",
 

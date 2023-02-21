2e295254f2a4eed55e1cc110a083a6abc4bf412f
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-02-20 23:37:59 -0600

    drawer bottom

--- App.tsx
+++ App.tsx
@@ -1,123 +1,127 @@
+import { ActionSheetProvider } from "@expo/react-native-action-sheet";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { PortalHost, PortalProvider } from "@gorhom/portal";
 import { Fragment, Suspense, useRef } from "react";
 import { Button, Dimensions, I18nManager, Text, View } from "react-native";
 import {
-  DrawerLayout,
-  GestureHandlerRootView,
-  TouchableOpacity,
+	DrawerLayout,
+	GestureHandlerRootView,
+	TouchableOpacity,
 } from "react-native-gesture-handler";
-
 import {
-  NativeRouter,
-  NavigateFunction,
-  Route,
-  Routes,
-  useNavigate,
+	NativeRouter,
+	NavigateFunction,
+	Route,
+	Routes,
+	useNavigate,
 } from "react-router-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import { DEVICE_WIDTH } from "./mobile/utils/constants";
 import { APP_LINKS } from "./routes";
 
 import "./styles";
-import { PortalHost, PortalProvider } from "@gorhom/portal";
-import { DEVICE_WIDTH } from "./mobile/utils/constants";
 
 export default function App() {
-  return (
-    <GestureHandlerRootView style={{ flex: 1, backgroundColor: "#cece" }}>
-      <PortalProvider>
-        <NativeRouter>
-          <Suspense>
-            <RoutesConfig />
-          </Suspense>
-        </NativeRouter>
-      </PortalProvider>
-    </GestureHandlerRootView>
-  );
+	return (
+		<GestureHandlerRootView style={{ flex: 1, backgroundColor: "#cece" }}>
+			<ActionSheetProvider>
+				<PortalProvider>
+					<NativeRouter>
+						<Suspense>
+							<RoutesConfig />
+						</Suspense>
+					</NativeRouter>
+				</PortalProvider>
+			</ActionSheetProvider>
+		</GestureHandlerRootView>
+	);
 }
 
 function RoutesConfig() {
-  const navigate = useNavigate();
-  const drawer = useRef<DrawerLayout>(null!);
-  return (
-    <DrawerLayout
-      ref={drawer}
-      /** @see https://reactnative.dev/docs/drawerlayoutandroid.html#drawerlockmode */
-      drawerLockMode="locked-open"
-      // ios only
-      enableTrackpadTwoFingerGesture
-      // common props
-      minSwipeDistance={320}
-      userSelect="text"
-      edgeWidth={100}
-      drawerWidth={DEVICE_WIDTH * 0.5}
-      drawerPosition={I18nManager.isRTL ? "right" : "left"}
-      drawerType="slide"
-      drawerBackgroundColor={"#b3b3b3"}
-      onDrawerSlide={(status: any) => console.log(status)}
-      renderNavigationView={() => sidebar(drawer, navigate)}
-    >
-      {openDrawer(drawer)}
-      {/* visual log */}
-      <View
-        className="flex items-center justify-center bg-purple-400">
-        <Text className="text-xl text-slate-100">
-          <PortalHost name="global-log" />
-        </Text>
-      </View>
+	const navigate = useNavigate();
+	const drawer = useRef<DrawerLayout>(null!);
+	return (
+		<DrawerLayout
+			ref={drawer}
+			/** @see https://reactnative.dev/docs/drawerlayoutandroid.html#drawerlockmode */
+			drawerLockMode="locked-open"
+			// ios only
+			enableTrackpadTwoFingerGesture
+			// common props
+			minSwipeDistance={320}
+			userSelect="text"
+			edgeWidth={100}
+			drawerWidth={DEVICE_WIDTH * 0.5}
+			drawerPosition={I18nManager.isRTL ? "right" : "left"}
+			drawerType="slide"
+			drawerBackgroundColor="#b3b3b3"
+			onDrawerSlide={(status: any) => console.log(status)}
+			renderNavigationView={() => sidebar(drawer, navigate)}
+		>
+			{openDrawer(drawer)}
+			{/* visual log */}
+			<View
+				className="flex items-center justify-center bg-purple-400">
+				<Text className="text-xl text-slate-100">
+					<PortalHost name="global-btn" />
+					<PortalHost name="global-log" />
+				</Text>
+			</View>
 
-      <Routes>
-        {APP_LINKS.map((link) => {
-          return (
-            <Fragment key={link.uuid}>
-              <Route path={link.path} element={link.element} />
-            </Fragment>
-          );
-        })}
-      </Routes>
-    </DrawerLayout>
-  );
+			<Routes>
+				{APP_LINKS.map((link) => {
+					return (
+						<Fragment key={link.uuid}>
+							<Route path={link.path} element={link.element} />
+						</Fragment>
+					);
+				})}
+			</Routes>
+		</DrawerLayout>
+	);
 }
 
 function sidebar(
-  drawer: React.MutableRefObject<DrawerLayout>,
-  navigate: NavigateFunction,
+	drawer: React.MutableRefObject<DrawerLayout>,
+	navigate: NavigateFunction,
 ): React.ReactNode {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignContent: "center",
-      }}
-    >
-      {closeDrawer(drawer)}
-      <Text style={{ textAlign: "center" }}>Links</Text>
-      {APP_LINKS.map((link) => {
-        return (
-          <Fragment key={link.uuid}>
-            <Button onPress={() => navigate(link.path)} title={link.alias} />
-          </Fragment>
-        );
-      })}
-    </View>
-  );
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: "center",
+				alignContent: "center",
+			}}
+		>
+			{closeDrawer(drawer)}
+			<Text style={{ textAlign: "center" }}>Links</Text>
+			{APP_LINKS.map((link) => {
+				return (
+					<Fragment key={link.uuid}>
+						<Button onPress={() => navigate(link.path)} title={link.alias} />
+					</Fragment>
+				);
+			})}
+		</View>
+	);
 }
 
 function openDrawer(drawer: React.MutableRefObject<DrawerLayout>) {
-  return (
-    <View>
-      <TouchableOpacity onPress={() => drawer.current.openDrawer()}>
-        <MaterialCommunityIcons name="backburger" size={64} color="black" />
-      </TouchableOpacity>
-    </View>
-  );
+	return (
+		<View>
+			<TouchableOpacity onPress={() => drawer.current.openDrawer()}>
+				<MaterialCommunityIcons name="backburger" size={64} color="black" />
+			</TouchableOpacity>
+		</View>
+	);
 }
 
 function closeDrawer(drawer: React.MutableRefObject<DrawerLayout>) {
-  return (
-    <View>
-      <TouchableOpacity onPress={() => drawer.current.closeDrawer()}>
-        <MaterialCommunityIcons name="backburger" size={64} color="black" />
-      </TouchableOpacity>
-    </View>
-  );
+	return (
+		<View>
+			<TouchableOpacity onPress={() => drawer.current.closeDrawer()}>
+				<MaterialCommunityIcons name="backburger" size={64} color="black" />
+			</TouchableOpacity>
+		</View>
+	);
 }

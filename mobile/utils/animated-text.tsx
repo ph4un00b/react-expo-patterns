1c23f019a3711052dc00a0186f20686942072ddf
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-03-06 16:17:13 -0600

    format

--- mobile/utils/animated-text.tsx
+++ mobile/utils/animated-text.tsx
@@ -7,48 +7,49 @@ import Animated, { useAnimatedProps } from "react-native-reanimated";
  * @see https://github.com/wcandillon/react-native-redash/blob/master/src/ReText.tsx
  */
 const styles = StyleSheet.create({
-    baseStyle: {
-        color: "black",
-    },
+	baseStyle: {
+		color: "black",
+	},
 });
+
 Animated.addWhitelistedNativeProps({ text: true });
 
 interface TextProps extends Omit<TextInputProps, "value" | "style"> {
-    text: Animated.SharedValue<string | number>;
-    style?: Animated.AnimateProps<RNTextProps>["style"];
+	text: Animated.SharedValue<string | number>;
+	style?: Animated.AnimateProps<RNTextProps>["style"];
 }
 
 const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
 
 export function AnimatedText(props: TextProps) {
-    const { style, text, ...rest } = props;
+	const { style, text, ...rest } = props;
 
-    const animatedProps = useAnimatedProps(() => {
-        // console.log(text.value)
-        const value = typeof text.value == "string"
-            ? text.value
-            : text.value.toFixed(2);
+	const animatedProps = useAnimatedProps(() => {
+		// console.log(text.value)
+		const value = typeof text.value == "string"
+			? text.value
+			: text.value.toFixed(2);
 
-        if (Platform.OS != "web") {
-            return {
-                text: value,
-            } as any;
-        } else {
-            return {
-                value: value,
-                // Here we use any because the text prop is not available in the type
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            } as any;
-        }
-    });
-    return (
-        <AnimatedTextInput
-            underlineColorAndroid="red"
-            editable={false}
-            value={typeof text.value == "string" ? text.value : text.value.toString()}
-            style={[styles.baseStyle, style || undefined]}
-            {...rest}
-            animatedProps={animatedProps}
-        />
-    );
+		if (Platform.OS != "web") {
+			return {
+				text: value,
+			} as any;
+		} else {
+			return {
+				value,
+				// Here we use any because the text prop is not available in the type
+				// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			} as any;
+		}
+	});
+	return (
+		<AnimatedTextInput
+			underlineColorAndroid="red"
+			editable={false}
+			value={typeof text.value == "string" ? text.value : text.value.toString()}
+			style={[styles.baseStyle, style || undefined]}
+			{...rest}
+			animatedProps={animatedProps}
+		/>
+	);
 }
